8ce706c9958100af5662a54927a3c82ddeca2551
DinaMartinezpantoja15 <dinamartinezpant@gmail.com>
2024-04-24 15:07:12 -0500

    se finalizan las vistas de los pacientes

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using TurnosLaM.Filters;
+using Microsoft.AspNetCore.Mvc;
+using TurnosLaM.Models;
+
+
+
+namespace TurnosLaM.Controllers{
+    public class UsersController : Controller{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+        public IActionResult Documento()
+    {
+        return View();
+    }
+    public IActionResult Servicios()
+    {
+        return View();
+    }
+
+
+}
+}
+
+
+
+
