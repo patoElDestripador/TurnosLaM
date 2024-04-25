5a5a9ff9a2a6fb064480de7501c949ac34d26b2d
Juan Rios <juanpr3112@gmail.com>
2024-04-25 15:42:20 -0500

    Cambios

--- Models/Shift.cs
+++ Models/Shift.cs
@@ -1,11 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 namespace TurnosLaM.Models
 {
-    public class ShiftModel
-    {
-        public int Id { get; set;}
-        public int PatientId { get; set; }  
-        public int ServiceId { get; set; }  
-        public DateTime CreationDate { get; set; }  
-    }
+public class ShiftModel
+        {
+                public int Id { get; set; }
+                public int PatientId { get; set; }
+                public int ServiceId { get; set; }
+                public DateTime CreationDate { get; set; }
+        }
 }
